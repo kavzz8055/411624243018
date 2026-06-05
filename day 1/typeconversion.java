f2cfdcaad50bdebe888cfab23f9c7b4cf8e05f8b
kavyashri.k <kavyashri.ks8055@gmail.com>
2026-06-05 12:19:36 +0530

    Hello world

---
+++ day 1/typeconversion.java
@@ -0,0 +1,9 @@
+public class typeconversion{
+    public static void main(String[] args){
+        int a=201;
+        byte b=(byte)a;
+        System.out.println(+b);
+
+
+    }
+}
---
+++ day 2/test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter hall ticket no:");
+        int number=sc.nextInt();
+        System.out.println("Enter mobile number:");
+        int mob=sc.nextInt();
+        System.out.println("Hall no:" + number + "\n")
+                          ("Mobile no:" + mob + "\n");
+    }
+    
+}
